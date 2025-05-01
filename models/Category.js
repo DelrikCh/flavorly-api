34e120aa38b463b19d014bc52ea53e61a2aecbd5
ekaterinakur <ekaterina1.kur@gmail.com>
2025-05-01 20:52:09 +0300

    prettier

--- models/Category.js
+++ models/Category.js
@@ -1,16 +1,19 @@
 import { DataTypes } from 'sequelize';
 import { sequelize } from '../db/index.js';
 
-const Category = sequelize.define('Category', {
-  id: {
-    type: DataTypes.STRING,
-    primaryKey: true,
+const Category = sequelize.define(
+  'Category',
+  {
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-}, {
+  {
     tableName: 'categories',
     timestamps: false,
   }
