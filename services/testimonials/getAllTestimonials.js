2a134687ae9ab15fccdf7f7b6f7db1b25233fedd
Rodion <rodion.groshev@gmail.com>
2025-05-03 14:01:05 +0200

    added owner name to response

--- services/testimonials/getAllTestimonials.js
+++ services/testimonials/getAllTestimonials.js
@@ -1,7 +1,16 @@
 import Testimonial from '../../models/Testimonial.js';
+import User from '../../models/User.js';
 
 const getAllTestimonials = async () => {
-  return Testimonial.findAll();
+  return Testimonial.findAll({
+    include: [
+      {
+        model: User,
+        as: 'owner',
+        attributes: ['name'],
+      },
+    ],
+  });
 };
 
 export default getAllTestimonials;
