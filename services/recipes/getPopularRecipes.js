7504d67f88b6ad4fb852bf2452dfdc752c338c60
Kyryl Potapenko <kpotapenko@yahoo.com>
2025-05-05 22:52:35 +0300

    Update /recipes endpoint so it returns owner's name and avatar #53

--- services/recipes/getPopularRecipes.js
+++ services/recipes/getPopularRecipes.js
@@ -1,12 +1,17 @@
 import models from '../../models/index.js';
 
 const findPopularRecipes = async ({ limit }) => {
-  const result = await models.Recipe.findAll({
+  return await models.Recipe.findAll({
     order: [['favoritesCount', 'DESC']],
     limit,
+    include: [
+      {
+        model: models.User,
+        as: 'owner',
+        attributes: ['name', 'avatar'],
+      },
+    ],
   });
-
-  return result;
 };
 
 export default findPopularRecipes;
--- services/recipes/getRecipe.js
+++ services/recipes/getRecipe.js
@@ -1,9 +1,17 @@
 import Recipe from '../../models/Recipe.js';
 import Ingredient from '../../models/Ingredient.js';
+import User from '../../models/User.js';
 
 const getRecipe = async (id) => {
   return await Recipe.findByPk(id, {
-    include: [Ingredient],
+    include: [
+      Ingredient,
+      {
+        model: User,
+        as: 'owner',
+        attributes: ['name', 'avatar'],
+      },
+    ],
   });
 };
 
